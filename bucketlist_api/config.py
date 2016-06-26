3cadb71cd0518645ab9f0f6df101db8466e2dbc7
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-06-26 16:24:15 +0100

    [feature] create app configuration

---
+++ bucketlist_api/config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config(object):
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+    ERROR_404_HELP = False
+    SECRET_KEY = 'randomthoughscomingtomymind'
+
+
+class ProdConfig(Config):
+    """Production configuration."""
+    ENV = 'prod'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'production-url')
+
+
+class DevConfig(Config):
+    """Development configuration."""
+    ENV = 'dev'
+    DEBUG = True
+    DB_NAME = 'dev.db'
+    DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
+
+
+class TestConfig(Config):
+    """Test configuration."""
+    ENV = 'test'
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
