31f912e565e29ca86e98c99e50390c0430b76672
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-06-27 15:41:51 +0100

    [feature] add create_app for configuration setting

---
+++ bucketlist_api/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+def create_app(ConfigObj):
+    """Create an instance of Flask with the right
+    configuration.
+
+    Arguments:
+        env:String specifying the development enviroment
+    Return:
+        Flask App instance
+    """
+    app = Flask(__name__)
+    app.config.from_object(ConfigObj)
+    api_blueprint = Blueprint("api", __name__, url_prefix='/')
+    app.register_blueprint(api_blueprint)
+    return app
+
+db = SQLAlchemy()
