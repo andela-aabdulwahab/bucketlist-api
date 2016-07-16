8267c8e65908f25f73091d5183efe456b4892753
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-07-16 22:03:26 +0100

    [feature] restructure the model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,8 +14,7 @@ class Testmodels(unittest.TestCase):
         self.app = create_app(TestConfig)
         self.app.app_context().push()
         db.create_all()
-        self.user = User(username="tester")
-        self.user.hash_password("wahab")
+        self.user = User(username="tester", password="wahab")
         db.session.add(self.user)
         db.session.commit()
         self.token = self.user.generate_auth_token()
@@ -42,10 +41,6 @@ class Testmodels(unittest.TestCase):
         time.sleep(2)
         self.assertFalse(self.user.verify_token(token))
 
-    def test_get_user_with_token(self):
-        user = User.get_user_with_token(self.token)
-        self.assertEqual(user.id, self.user.id)
-
     def test_user_exist(self):
         self.assertTrue(User.user_exist("tester"))
 
@@ -63,20 +58,6 @@ class TestBucketListModels(unittest.TestCase):
 
     def test_bucketlist_init(self):
         self.assertGreater(self.bucketlist.id, 0)
-
-    def test_build_bucketlist(self):
-        bucketlist_dict = BucketList.build_bucketlist([self.bucketlist])
-        bucketlist_name = bucketlist_dict[self.bucketlist.id]["name"]
-        self.assertTrue(type(bucketlist_dict) is dict)
-        self.assertGreater(len(bucketlist_dict), 0)
-        self.assertEqual(bucketlist_name, "Travel the World")
-
-    def test_get_bucketlist_id(self):
-        bucketlists = BucketList.get_bucketlist(id=self.bucketlist.id)
-        self.assertTrue(type(bucketlists) is list)
-        self.assertEqual(len(bucketlists), 2)
-        self.assertIsNone(bucketlists[1])
-        self.assertTrue(type(bucketlists[0]) is dict)
 
 
 class TestBucketListItemModels(unittest.TestCase):
@@ -98,15 +79,4 @@ class TestBucketListItemModels(unittest.TestCase):
         db.session.commit()
 
     def test_bucketlist_item_init(self):
-        self.assertTrue(self.bucketlist_item.id > 0)
-
-    def test_build_item_list(self):
-        items = BucketListItem.query.all()
-        built_item = BucketListItem.build_item_list(items)
-        self.assertGreater(len(built_item), 1)
-        self.assertEqual(built_item[0]['name'], self.bucketlist_item.name)
-
-    def test_get_bucketlist_items(self):
-        items = BucketList.get_bucketlist_items(self.bucketlist_item
-                                                .bucketlist_id)
-        self.assertGreater(len(items), 0)
+        self.assertGreater(self.bucketlist_item.id, 0)
